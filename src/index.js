03e1382fdb97c4dedcf65fda51c394d5f60fdb86
Igor Voitov <igorgrime@gmail.com>
2023-08-05 11:46:29 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+
+    let arrayOfIncomingNumbers = Array.from(String(Math.abs(n)));
+    let reverseArrayOfNumbers = [];
+
+    for (let i = arrayOfIncomingNumbers.length; i > 0; i--) {
+        reverseArrayOfNumbers.push(arrayOfIncomingNumbers[i - 1]);
+    }
+
+    return reverseArrayOfNumbers.join('')
 }
